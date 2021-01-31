4049f2de64ac9798bdbb998634f43773f91ad2ca
Jane.Gao <hayond@qq.com>
2021-01-31 12:58:37 +0800

    m

---
+++ packages/styles/src/index.js
@@ -0,0 +1,5 @@
+import { createStyled, createCss } from '@emotion/primitives-core'
+import { StyleSheet } from 'react-native-web'
+
+export const styled = createStyled(StyleSheet)
+export const css = createCss(StyleSheet)
